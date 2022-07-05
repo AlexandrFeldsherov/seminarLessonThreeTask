b8a9b9904541a9f315691117c452cac43f7e62f4
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-07-06 00:29:08 +0300

    V005

--- task019/Program.cs
+++ task019/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+bool DeterminingNumber(string num)
+{
+    foreach (var item in num)
+    {
+        if (!char.IsDigit(item))
+            return false;
+    }
+    return true;
+}
+int Long(string lon)
+{
+    int i = 0;
+    foreach (var item in lon)
+    {
+        i++;
+    }
+    return i;
+}
+string Palindrome(string number)
+{
+    int i = 0;
+    foreach (var item in number)
+    {
+        i++;
+    }
+    int ind = 0;
+    int inde = i - 1;
+    string[] str = new string[i];
+    string[] rts = new string[i];
+    foreach (var item in number)
+    {
+        // str[ind] = Convert.ToString(item);
+        rts[inde] = Convert.ToString(item);
+        ind++;
+        inde--;
+    }
+    //Console.WriteLine("[{0}]", string.Join(", ", rts));
+    // Console.WriteLine("[{0}]", string.Join(", ", str));
+    int a = 0;
+    string b = "";
+    foreach (var item in number)
+    {
+        if (rts[a] == Convert.ToString(item)) b = "да";
+        else return "нет";
+        a++;
+    }
+    return "да";
+}
+Console.Clear();
+string outputResult;
+Console.Write("Введите пятизначное число, положительное число: ");
+string number = Console.ReadLine();
+int longStringNumber = Long(number);//длинна вводимой строки
+bool determiningNumber = DeterminingNumber(number);//определение ввод чисел
+if (determiningNumber != true)
+{
+    Console.WriteLine("Ввод не соответствует условию");
+    Console.Write("Введите пятизначное число, положительное число: ");
+    number = Console.ReadLine();
+}
+else if (longStringNumber != 5)
+{
+    {
+        Console.WriteLine("Ввод не соответствует условию");
+        Console.Write("Введите пятизначное число, положительное число: ");
+        number = Console.ReadLine();
+    }
+}
+else 
+{
+outputResult = Palindrome(number);//метод определяет палинром ли строка
+Console.WriteLine(outputResult);
+}
